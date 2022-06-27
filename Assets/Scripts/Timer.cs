11189ea76642add2f7bef0753522786388ab07ba
Tugbay Atilla <Tugbay Atilla tatilla@igus.net>
2022-06-27 23:31:33 +0200

    feat: add timer fill fraction

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,21 +7,31 @@ public class Timer : MonoBehaviour
     [SerializeField] private float timeToCompleteQuestion = 30f;
     [SerializeField] private float timeToShowCorrectAnswer = 10f;
 
+    public bool loadNextQuestion;
     public bool isAnsweringQuestion = false;
-    
+    public float fillFraction;
     private float timerValue;
     void Update()
     {
         UpdateTimer();
     }
 
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+    
     void UpdateTimer()
     {
         timerValue -= Time.deltaTime;
 
         if (isAnsweringQuestion)
         {
-            if (timerValue <= 0)
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
             {
                 isAnsweringQuestion = false;
                 timerValue = timeToShowCorrectAnswer;
@@ -29,10 +39,15 @@ public class Timer : MonoBehaviour
         }
         else
         {
-            if (timerValue <= 0)
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
             {
                 isAnsweringQuestion = true;
                 timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
             }
         }
        
